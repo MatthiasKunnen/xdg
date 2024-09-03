dec82fdcb248e1693b63226fc2eb5bb56ac321cb
Matthias Kunnen <matthias.kunnen@gmail.com>
2024-09-04 00:11:18 +0200

    Implement magic detection of desktop files

---
+++ desktop/magic.go
@@ -0,0 +1,95 @@
+package desktop
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+)
+
+const (
+	magicDefault = iota
+	magicSkipUntilLf
+)
+
+// MagicIsDesktopFile returns true if the content is likely a desktop file.
+// The content is checked according to the [desktop entry format] spec.
+//
+// [desktop entry format]: https://specifications.freedesktop.org/desktop-entry-spec/1.5/basic-format.html
+func MagicIsDesktopFile(reader io.Reader) (bool, error) {
+	expectedEntry := "Desktop Entry]"
+	utf8BomHeader := []byte{0xEF, 0xBB, 0xBF}
+
+	r := bufio.NewReader(reader)
+
+	maybeBom, err := r.Peek(len(utf8BomHeader))
+	if err != nil {
+		return false, nil
+	}
+
+	if bytes.Equal(maybeBom, utf8BomHeader) {
+		_, err := r.Discard(len(utf8BomHeader))
+		if err != nil {
+			return false, nil
+		}
+	}
+
+	var status int
+
+	for {
+		readRune, _, err := r.ReadRune()
+		switch {
+		case readRune == unicode.ReplacementChar:
+			// Desktop file must be UTF-8
+			return false, nil
+		case err != nil:
+			return false, nil
+		}
+
+		switch status {
+		case magicDefault:
+			switch readRune {
+			case '#':
+				status = magicSkipUntilLf
+				continue
+			case '\n':
+				continue
+			case '[':
+				deBuffer := make([]byte, len(expectedEntry))
+				_, err := io.ReadFull(r, deBuffer)
+				if err != nil {
+					return false, nil
+				}
+
+				return expectedEntry == string(deBuffer), nil
+			default:
+				return false, nil
+			}
+		case magicSkipUntilLf:
+			if readRune == '\n' {
+				status = magicDefault
+			}
+			continue
+		}
+	}
+}
+
+// MagicIsDesktopFilePath returns true if the file at the given path is likely a desktop file.
+// The file is checked according to the [desktop entry spec].
+//
+// [desktop entry spec]: https://specifications.freedesktop.org/desktop-entry-spec/1.5/basic-format.html
+func MagicIsDesktopFilePath(path string) (bool, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return false, fmt.Errorf(
+			"failed to open file '%s' to check if it a desktop file. %w",
+			path,
+			err,
+		)
+	}
+
+	return MagicIsDesktopFile(file)
+}
---
+++ desktop/magic_test.go
@@ -0,0 +1,89 @@
+package desktop
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestMagicIsDesktopFileEmpty(t *testing.T) {
+	isDesktopFile, err := MagicIsDesktopFile(strings.NewReader(""))
+
+	if err != nil {
+		t.Fatalf("Empty file should not result in an error: %v", err)
+	}
+
+	if isDesktopFile {
+		t.Fatalf("Empty file should not be recognized as a desktop file")
+	}
+}
+
+func TestMagicIsDesktopFileSuccessNoBom(t *testing.T) {
+	isDesktopFile, err := MagicIsDesktopFile(strings.NewReader(`[Desktop Entry]
+Name=Hello
+`))
+
+	if err != nil {
+		t.Fatalf("Correct file should not result in an error: %v", err)
+	}
+
+	if !isDesktopFile {
+		t.Fatalf("File should be recognized as a desktop file")
+	}
+}
+
+func TestMagicIsDesktopFileSuccessWithBom(t *testing.T) {
+	isDesktopFile, err := MagicIsDesktopFile(strings.NewReader(
+		"\xef\xbb\xbf[Desktop Entry]\nName=Hello",
+	))
+
+	if err != nil {
+		t.Fatalf("Correct file with UTF-8 BOM should not result in an error: %v", err)
+	}
+
+	if !isDesktopFile {
+		t.Fatalf("Correct file with UTF-8 BOM should be recognized as a desktop file")
+	}
+}
+
+func TestMagicIsDesktopFileIncorrectBom(t *testing.T) {
+	isDesktopFile, err := MagicIsDesktopFile(strings.NewReader(
+		"\xef\xbb\xbe[Desktop Entry]\nName=Hello",
+	))
+
+	if err != nil {
+		t.Fatalf("Correct file with incorrect BOM should not result in an error: %v", err)
+	}
+
+	if isDesktopFile {
+		t.Fatalf("Correct file with incorrect BOM should not be recognized as a desktop file")
+	}
+}
+
+func TestMagicIsDesktopFileSuccessWithComments(t *testing.T) {
+	isDesktopFile, err := MagicIsDesktopFile(strings.NewReader(`# Hello there # Maybe
+[Desktop Entry]
+Name=Hello
+`))
+	if err != nil {
+		t.Fatalf("Correct file with comments should not result in an error: %v", err)
+	}
+
+	if !isDesktopFile {
+		t.Fatalf("Correct file with comments should be recognized as a desktop file")
+	}
+}
+
+func TestMagicIsDesktopFileSuccessWithNewlines(t *testing.T) {
+	isDesktopFile, err := MagicIsDesktopFile(strings.NewReader(`
+
+[Desktop Entry]
+Name=Hello
+`))
+	if err != nil {
+		t.Fatalf("Correct file with newlines not result in an error: %v", err)
+	}
+
+	if !isDesktopFile {
+		t.Fatalf("Correct file with newlines should be recognized as a desktop file")
+	}
+}
